ae02112d0ed1371d36621942aa1b353172616a80
wonrax <hahuylonghai2012@gmail.com>
2023-08-07 18:00:26 +0700

    continue work on plugin system

--- architect/plugin-system/src/main.rs
+++ architect/plugin-system/src/main.rs
@@ -1,46 +1,93 @@
-use std::collections::HashMap;
+use std::{
+    collections::HashMap, error::{self, Error},
+};
 
+#[derive(Debug)]
 enum PluginState {
     Enabled,
     Started,
     Disabled, // or stopped
-    Destroyed
+    Destroyed,
 }
 
 struct PluginManager {
     plugin_states: HashMap<String, PluginState>,
-    plugins: Vec<Box<dyn IPlugin>>
+    plugins: HashMap<String, Box<dyn IPlugin>>,
 }
 
-trait IPluginManager {
-    fn register_plugin<'a>(&mut self, p: impl IPlugin + 'static) -> Result<&Box<dyn IPlugin>, String>;
-}
+impl PluginManager {
+    fn register_plugin(
+        &mut self,
+        mut p: impl IPlugin + 'static,
+    ) -> Result<&Box<dyn IPlugin>, String> {
+        let type_name = p.get_name().to_string();
 
-impl IPluginManager for PluginManager {
-    fn register_plugin(&mut self, p: impl IPlugin + 'static) -> Result<&Box<dyn IPlugin>, String> {
-        let type_name = p.get_name();
-        if self.plugin_states.contains_key(type_name) {
+        if self.plugin_states.contains_key(&type_name) {
             return Err("Plugin already exists".into());
         }
 
-        self.plugin_states.insert(type_name.into(), PluginState::Enabled);
+        p.on_enabled();
 
         let _p = Box::new(p);
-        self.plugins.push(_p);
+        self.plugins.insert(type_name.clone(), _p);
 
-        Ok(self.plugins.last().unwrap())
+        self.plugin_states
+            .insert(type_name.clone(), PluginState::Enabled);
+
+        match self.plugins.get(&type_name) {
+            Some(plugin_ref) => Ok(plugin_ref),
+            None => Err("smth went wrong bruh".into()),
+        }
+    }
+
+    fn start_plugin(&mut self, plugin_name: String) -> Result<(), Box<dyn Error>> {
+        match self.plugin_states.get(&plugin_name) {
+            Some(state) => match state {
+                PluginState::Enabled => match self.plugins.get_mut(&plugin_name) {
+                    Some(p) => {
+                        p.on_start();
+                        self.plugin_states.insert(plugin_name, PluginState::Started);
+                        Ok(())
+                    }
+                    _ => Error::new,
+                },
+                _ => ()
+            },
+            _ => (),
+        }
+    }
+
+    fn disable_plugin(&mut self, plugin_name: String) {
+        match self.plugins.get_mut(&plugin_name) {
+            Some(p) => {
+                p.on_disabled();
+                self.plugin_states
+                    .insert(plugin_name, PluginState::Disabled);
+            }
+            _ => (),
+        }
+    }
+
+    fn destroy_plugin(&mut self, plugin_name: String) {
+        match self.plugins.get_mut(&plugin_name) {
+            Some(p) => {
+                p.on_destroy();
+                self.plugin_states
+                    .insert(plugin_name, PluginState::Destroyed);
+            }
+            _ => (),
+        }
     }
 }
 
 struct APlugin {}
-
 impl IPlugin for APlugin {}
 
 trait IPlugin {
-    fn on_enabled(&self) {}
-    fn on_start(&self) {}
-    fn on_disabled(&self) {}
-    fn on_destroy(&self) {}
+    fn on_enabled(&mut self) {}
+    fn on_start(&mut self) {}
+    fn on_disabled(&mut self) {}
+    fn on_destroy(&mut self) {}
     fn get_name(&self) -> &'static str {
         std::any::type_name::<Self>()
     }
@@ -49,11 +96,17 @@ trait IPlugin {
 fn main() {
     let mut pm = PluginManager {
         plugin_states: HashMap::new(),
-        plugins: Vec::new()
+        plugins: HashMap::new(),
     };
 
     match pm.register_plugin(APlugin {}) {
         Ok(p) => println!("Registered {}", p.get_name()),
         Err(err) => println!("Error: {}", err),
     }
+    match pm.register_plugin(APlugin {}) {
+        Ok(p) => println!("Registered {}", p.get_name()),
+        Err(err) => println!("Error: {}", err),
+    }
+
+    // println!("{:?}", pm.plugin_states.get(&Box::new(APlugin{})));
 }
