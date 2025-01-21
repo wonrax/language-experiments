0786d662541988138a89a98a2adaca6555b7b76c
wonrax <hahuylonghai2012@gmail.com>
2025-01-21 16:20:26 +0700

    haskell: implement tower of hanoi

--- challenge/cis1940/src/Chap1.hs
+++ challenge/cis1940/src/Chap1.hs
@@ -16,12 +16,18 @@ module Chap1 where
 
 validateCreditCardNumber :: Integer -> Bool
 validateCreditCardNumber n
-  | n <= 0 = False
-  | otherwise =
-      let digits = go n []
-          doubled = zipWith (*) (cycle [1, 2]) (reverse digits)
-          summed = sum [if x > 9 then x `div` 10 + x `mod` 10 else x | x <- doubled]
-       in summed `mod` 10 == 0
+    | n <= 0 = False
+    | otherwise =
+        let digits = go n []
+            doubled = zipWith (*) (cycle [1, 2]) (reverse digits)
+            summed = sum [if x > 9 then x `div` 10 + x `mod` 10 else x | x <- doubled]
+         in summed `mod` 10 == 0
   where
     go 0 acc = acc
     go x acc = go (x `div` 10) ((x `mod` 10) : acc)
+
+type Peg = String
+hanoi :: Integer -> Peg -> Peg -> Peg -> [(Peg, Peg)]
+hanoi 0 _ _ _ = []
+hanoi 1 a b _ = [(a, b)]
+hanoi n source target aux = hanoi (n - 1) source aux target ++ [(source, target)] ++ hanoi (n - 1) aux target source
--- challenge/cis1940/test/Chap1Spec.hs
+++ challenge/cis1940/test/Chap1Spec.hs
@@ -10,3 +10,8 @@ spec = do
       validateCreditCardNumber 4012888888881881 `shouldBe` True
     it "should return False for 4012888888881882" $ do
       validateCreditCardNumber 4012888888881882 `shouldBe` False
+  describe "hanoi tower" $ do
+    it "hanoi 2" $ do
+      hanoi 2 "a" "b" "c" `shouldBe` [("a", "c"), ("a", "b"), ("c", "b")]
+    it "hanoi 3" $ do
+      hanoi 3 "a" "b" "c" `shouldBe` [("a", "b"), ("a", "c"), ("b", "c"), ("a", "b"), ("c", "a"), ("c", "b"), ("a", "b")]
